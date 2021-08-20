d84fe1fae6492a826de03552d447460c7fb81673
Gui Pereira <guilhermepdo1508@gmail.com>
2021-08-20 18:27:04 -0300

    Updates

---
+++ precache-manifest.fa831ac7e102e5093554d5a5c8267fcc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d7ee6e2ef4091b384821e1d166f431",
+    "url": "/receitas/index.html"
+  },
+  {
+    "revision": "1e7d42ed554a8e275ee7",
+    "url": "/receitas/static/css/main.779ef7ff.chunk.css"
+  },
+  {
+    "revision": "ab05522aea9c3ac022c0",
+    "url": "/receitas/static/js/2.2d6a26dd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/receitas/static/js/2.2d6a26dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7d42ed554a8e275ee7",
+    "url": "/receitas/static/js/main.a4ec66ef.chunk.js"
+  },
+  {
+    "revision": "785a664b2d0c318963a7",
+    "url": "/receitas/static/js/runtime-main.6be6f397.js"
+  },
+  {
+    "revision": "fba924b8a6291e89709a6a5e76b86bf0",
+    "url": "/receitas/static/media/backgroundLogin.fba924b8.svg"
+  },
+  {
+    "revision": "8e74f65e0e1365423f9576c1a929f753",
+    "url": "/receitas/static/media/blackHeartIcon.8e74f65e.svg"
+  },
+  {
+    "revision": "970ab03ec5032cccf3431b9a54f2d8bc",
+    "url": "/receitas/static/media/blackHeartIcon.970ab03e.svg"
+  },
+  {
+    "revision": "5457b0b901e84c28b35efcd0522eaee8",
+    "url": "/receitas/static/media/drinkIcon.5457b0b9.svg"
+  },
+  {
+    "revision": "4dfb08013d3cf1ba450acdb217e759f6",
+    "url": "/receitas/static/media/exploreIcon.4dfb0801.svg"
+  },
+  {
+    "revision": "f0b0404bf35307ad0cb6ce5f2ce3c3e6",
+    "url": "/receitas/static/media/logo.f0b0404b.svg"
+  },
+  {
+    "revision": "4a7c882058b0b7a8f83c04c5e82d6a1b",
+    "url": "/receitas/static/media/mealIcon.4a7c8820.svg"
+  },
+  {
+    "revision": "2ef55437e45ea8df19aca8d88d46f046",
+    "url": "/receitas/static/media/profileIcon.2ef55437.svg"
+  },
+  {
+    "revision": "1ad5c2571ea99bf87442c8a8738d9d67",
+    "url": "/receitas/static/media/searchIcon.1ad5c257.svg"
+  },
+  {
+    "revision": "42aaa8b47c9cc6674641fea7e685e93d",
+    "url": "/receitas/static/media/shareIcon.42aaa8b4.svg"
+  },
+  {
+    "revision": "bdf82e3ec3b491642076b3be32beae7d",
+    "url": "/receitas/static/media/shareIcon.bdf82e3e.svg"
+  },
+  {
+    "revision": "09cd27b2ebe695a5a3c0b2a64ddea1d8",
+    "url": "/receitas/static/media/whiteHeartIcon.09cd27b2.svg"
+  },
+  {
+    "revision": "0ef4bd6035c31308b1361d23cee6ecfe",
+    "url": "/receitas/static/media/whiteHeartIcon.0ef4bd60.svg"
+  }
+]);
